6d70264009ecf28ea68d24cc91f9a6efb7aa98d8
Ezra Onyinkwa <139281995+ezraonyinkwa@users.noreply.github.com>
2026-04-21 16:55:39 +0300

    Standardize and clean customer datasets
    Standardized column names and cleaned customer datasets including gender, citizenship, and date of birth. Performed quality checks after each step.

---
+++ data_cleaning.py
@@ -0,0 +1,63 @@
+#--------------------Columns Standardization--------------------
+#Proper Case Standardization of the column names in the credit dataset
+credit.columns = credit.columns.str.strip().str.lower().str.replace(' ', '_')
+#Quality check after standardization
+credit.columns
+
+#Proper Case Standardization of the column names in the customer income dataset
+customer_income.columns = customer_income.columns.str.strip().str.lower().str.replace(' ', '_')
+#Quality check after standardization
+customer_income.columns
+
+#Proper Case Standardization of the column names in the customer gender dataset
+customer_gender.columns = customer_gender.columns.str.strip().str.lower().str.replace(' ', '_')
+#Quality check after standardization        
+customer_gender.columns
+
+#Proper Case Standardization of the column names in the customer dob dataset
+customer_dob.columns = customer_dob.columns.str.strip().str.lower().str.replace(' ','_')
+#Quality check after standardization
+customer_dob.columns
+
+#--------------------Customer Data gender dataset cleaning--------------------
+#Check unique values in the gender column and standardize them if necessary
+customer_gender['gender'].unique()
+#Ensure gender is standardized to Male,Female or Other
+customer_genderd=customer_gender.astype({'gender':'string'})
+customer_gender['gender'].fillna('Other',inplace=True)
+customer_gender['gender'].unique()
+customer_gender['gender'] = customer_gender['gender'].replace   ({'M':'Male','F':'Female','Unspecified':'Other'})
+
+#Formating of the citizenship column in the customer income dataset
+customer_gender['citizenship'].unique()
+customer_gender=customer_gender.astype({'citizenship':'string'})
+customer_gender['citizenship'] = customer_gender['citizenship'].str.strip().str.capitalize()
+customer_gender['citizenship'].fillna('Other',inplace=True)
+customer_gender['citizenship'] = customer_gender['citizenship'].replace({'Citizen':'Kenyan',})
+
+#Quality check after standardization
+customer_gender['gender'].unique()
+customer_gender['citizenship'].unique()
+
+
+#--------------------Customer Data DOB dataset cleaning--------------------
+#Check for age and group by age group
+customer_dob['provider'].unique()
+customer_dob['provider'].fillna('Other',inplace=True)
+customer_dob['provider']=customer_dob['provider'].str.strip().str.capitalize()
+
+#Quality check after standardization
+customer_dob['provider'].unique()
+
+# Datetimeconversion of the date of birth column
+customer_dob['date_of_birth'] = pd.to_datetime(customer_dob['date_of_birth'], errors='coerce') #--Change the date format to datetime and handle errors by coercing them to NaT
+#Quality check after format change-If it worked
+customer_dob['date_of_birth'].dtype
+
+
+
+#Drop Columns that are not needed for the analysis in the credit dataset
+credit.drop(columns=['snapshot_date'],inplace=True)
+credit.drop(columns=['unnamed:_28'],inplace=True)
+#Quality check after dropping the columns
+credit.columns  
