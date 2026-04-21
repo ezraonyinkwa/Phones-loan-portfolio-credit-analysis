ab9fff633d6b785e74b70c7180333ddfca30f2c5
Ezra Onyinkwa <139281995+ezraonyinkwa@users.noreply.github.com>
2026-04-21 17:01:35 +0300

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,12 @@
+#---------Portfolio Health-----------
+# 1.NPE Rate
+# 2.PAR 30 Rate 
+# 3.Impaired Rate
+# 4.Roll Rate (Active → PAR 30)
+# 5.Average Arrears per Impaired Account
+
+
+#Credit Outcomes × Customer Experience
+
+
+#
