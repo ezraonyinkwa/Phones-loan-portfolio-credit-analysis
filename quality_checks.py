c23776b8e83134284a5ce0d035c81a275ad0adc5
Ezra Onyinkwa <139281995+ezraonyinkwa@users.noreply.github.com>
2026-04-24 16:07:51 +0300

    Implement quality checks for customer datasets
    Added quality checks for various customer datasets including columns and unique values.

--- quality_checks.py
+++ quality_checks.py
@@ -1,1 +1,13 @@
-
+#Quality checks for column names
+credit.columns
+#Quality check for customer income dataset columns
+customer_income.columns
+#Quality check for customer gender dataset columns        
+customer_gender.columns
+#Quality check for customer_dob dataset columns
+customer_dob.columns
+#Check unique values in the gender
+customer_gender['gender'].unique()
+#Quality check after standardization
+customer_gender['gender'].unique()
+customer_gender['citizenship'].unique()
